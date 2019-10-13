c0a4485d92368bcd9e27f6e5e97c049abe09e03b
Blake VanLandingham <blakevanlan@gmail.com>
2019-10-13 15:30:04 -0400

    Fixes the specify action when randomizing (#21)

--- src/index-page.ts
+++ src/index-page.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import EnlargeButtonComponent from "./components/enlarge-button.vue";
 import PageComponent, { MenuItemType } from "./components/page.vue";
 import RandomizerComponent from "./components/randomizer.vue";
+import ReplaceSupplyCardModalComponent from "./components/replace-supply-card-modal.vue"
 import { store } from "./stores/index-store";
 import { initializeWindowListener } from "./setup";
 
@@ -12,6 +13,7 @@ new Vue({
   el: "#app",
   template: `
   <div>
+    <replace-supply-card-modal-component :key="this.$store.state.randomizer.kingdom.id" />
     <page-component :subtitle="subtitle" :selectedType="selectedType">
       <randomizer-component @specify-replacement="showReplaceModal = true" />
     </page-component>
@@ -26,6 +28,7 @@ new Vue({
   components: {
     "page-component": PageComponent,
     "randomizer-component": RandomizerComponent,
+    "replace-supply-card-modal-component": ReplaceSupplyCardModalComponent,
     "enlarge-button-component": EnlargeButtonComponent,
   },
 });
