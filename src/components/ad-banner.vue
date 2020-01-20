6890c21c03432aeefdb6be26a6808637fb9a9eee
Blake VanLandingham <blakevanlan@gmail.com>
2020-01-20 11:52:33 -0500

    Prepared for adding ads

--- src/components/ad-banner.vue
+++ src/components/ad-banner.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="ad-banner">
-    <div class="ad-text">
-      Interested in advertising here?
-    </div>
-    <a href="https://forms.gle/eE3HnhrVvjWrbCLS8"
-      class="standard-button standard-button--is-primary standard-button--small"
-    >
-      Get in touch!
-    </a>
   </div>
 </template>
 
@@ -22,29 +14,13 @@ Vue.component("ad-banner-component", AdBanner);
 
 <style>
 .ad-banner {
-  align-items: center;
-  background: rgba(2, 119, 158, 0.1);
-  border: 1px solid rgba(2, 119, 158, 0.7);
-  border-radius: 8px;
-  color: rgba(2, 119, 158, 1);
-  display: flex;
-  flex-direction: row;
-  font-family: 'Alegreya Sans', sans-serif;
-  font-size: 16px;
-  font-weight: 300;
-  justify-content: space-between;
-  margin: 42px 12px 12px 0;
-  padding: 8px 12px;
+  margin: 42px 0 12px 0;
 }
 
 @media (max-width: 750px) {
   .ad-banner {
-    margin-left: 12px;
+    margin-left: 4px;
+    margin-right: 4px;
   }
 }
-
-.link {
-
-}
-
 </style>
